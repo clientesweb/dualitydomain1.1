967fdd3d3bed2e254a4ff21b36758cf5c6d2c15f
clientesweb <alexisifran32@gmail.com>
2025-05-22 14:47:52 -0300

    Actualizar Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { navVariants } from "../utils/motion";
-import search from "../public/search.svg";
-import menu from "../public/menu.svg";
+import Image from "next/image"
+import { motion } from "framer-motion"
+import styles from "../styles"
+import { navVariants } from "../utils/motion"
+import search from "../public/search.svg"
+import menu from "../public/menu.svg"
 
 const Navbar = () => (
   // NOTE we can declare tags with motion like this for whichever tag we want to animate
@@ -24,22 +24,12 @@ const Navbar = () => (
       whileInView="show"
       className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
     >
-      <Image
-        src={search}
-        alt="search-Icon"
-        className="w-[24px] h-[24px] object-contain"
-      />
+      <Image src={search || "/placeholder.svg"} alt="search-Icon" className="w-[24px] h-[24px] object-contain" />
 
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white ">
-        METAVERSUS
-      </h2>
-      <Image
-        src={menu}
-        alt="menu-Icon"
-        className="w-[24px] h-[24px] object-contain"
-      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white ">INNOVALINE</h2>
+      <Image src={menu || "/placeholder.svg"} alt="menu-Icon" className="w-[24px] h-[24px] object-contain" />
     </motion.div>
   </nav>
-);
+)
 
-export default Navbar;
+export default Navbar

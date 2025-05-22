510e4be8e68675fec4e0f72d691aea2bf9f199fc
clientesweb <alexisifran32@gmail.com>
2025-05-22 15:00:01 -0300

    Actualizar Explore.jsx

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,13 +1,13 @@
-"use client";
-import { useState } from "react";
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
-import { TypingText, ExploreCard, TitleText } from "../components";
-import { exploreWorlds } from "../constants";
+"use client"
+import { useState } from "react"
+import { motion } from "framer-motion"
+import styles from "../styles"
+import { staggerContainer } from "../utils/motion"
+import { TypingText, ExploreCard, TitleText } from "../components"
+import { exploreWorlds } from "../constants"
 
 const Explore = () => {
-  const [activeCard, setActiveCard] = useState("world-2");
+  const [activeCard, setActiveCard] = useState("world-2")
   return (
     <section className={`${styles.paddings}`} id="explore">
       <motion.div
@@ -17,30 +17,16 @@ const Explore = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title="| The World" textStyles="text-center" />
-        <TitleText
-          title={
-            <>
-              Choose the world you want <br className="md:block hidden" /> to
-              explore{" "}
-            </>
-          }
-          textStyles="text-center"
-        />
+        <TypingText title="| ¿Qué hacemos?" textStyles="text-center" />
+        <TitleText title={<>Servicios Ofrecidos por Duality Domain</>} textStyles="text-center" />
         <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5 ">
           {exploreWorlds.map((world, index) => (
-            <ExploreCard
-              key={world.id}
-              {...world}
-              index={index}
-              active={activeCard}
-              handleClick={setActiveCard}
-            />
+            <ExploreCard key={world.id} {...world} index={index} active={activeCard} handleClick={setActiveCard} />
           ))}
         </div>
       </motion.div>
     </section>
-  );
-};
+  )
+}
 
-export default Explore;
+export default Explore

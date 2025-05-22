2ce001a8efa984cc11bc8ed84c3bd51c356141b6
clientesweb <alexisifran32@gmail.com>
2025-05-22 16:25:14 -0300

    Actualizar head.js

--- app/head.js
+++ app/head.js
@@ -1,9 +1,17 @@
 const Head = () => (
   <>
-    <title>Rohit's Metaversus</title>
+    <title>Duality Domain | Desarrollo Web Profesional</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta
+      name="description"
+      content="Duality Domain es una empresa de desarrollo web profesional ubicada en Villa del Dique, Córdoba, Argentina. Con presencia en LATAM. Se especializa en ofrecer soluciones digitales personalizadas que combinan creatividad y excelencia técnica para impulsar el crecimiento de los negocios."
+    />
+    <meta
+      name="keywords"
+      content="desarrollo web, diseño web, e-commerce, SEO, aplicaciones web, UX, Villa del Dique, Córdoba, Argentina, LATAM"
+    />
     <link rel="icon" href="/favicon.ico" />
   </>
-);
+)
 
-export default Head;
+export default Head

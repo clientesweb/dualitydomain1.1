952a9ee9e03c5b18a7eb3404b3fc4d78ab6d70c8
clientesweb <alexisifran32@gmail.com>
2025-05-22 16:30:18 -0300

    Actualizar WhatsNew.jsx

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,12 +1,12 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { fadeIn, staggerContainer, planetVariants } from "../utils/motion";
-import { TypingText, TitleText, NewFeatures } from "../components";
-import { newFeatures } from "../constants";
-import whats_new from "../public/whats-new.png";
+import Image from "next/image"
+import { motion } from "framer-motion"
+import styles from "../styles"
+import { fadeIn, staggerContainer, planetVariants } from "../utils/motion"
+import { TypingText, TitleText, NewFeatures } from "../components"
+import { newFeatures } from "../constants"
+import whats_new from "../public/whats-new.png"
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -16,12 +16,9 @@ const WhatsNew = () => (
       whileInView="show"
       className={`${styles.innerWidth} flex mx-auto w-full lg:flex-row flex-col gap-8`}
     >
-      <motion.div
-        variants={fadeIn("right", "tween", 0.2, 1)}
-        className="flex-[0.75] flex justify-center flex-col"
-      >
-        <TypingText title="| What's New?" />
-        <TitleText title={<>What's new about metaversus?</>} />
+      <motion.div variants={fadeIn("right", "tween", 0.2, 1)} className="flex-[0.75] flex justify-center flex-col">
+        <TypingText title="| ¿Por qué elegirnos?" />
+        <TitleText title={<>Ventajas de trabajar con Duality Domain</>} />
         <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
           {newFeatures.map((feature) => (
             <NewFeatures key={feature.title} {...feature} />
@@ -29,19 +26,16 @@ const WhatsNew = () => (
         </div>
       </motion.div>
 
-      <motion.div
-        variants={planetVariants("right")}
-        className={`flex-1 ${styles.flexCenter}`}
-      >
+      <motion.div variants={planetVariants("right")} className={`flex-1 ${styles.flexCenter}`}>
         <Image
-          src={whats_new}
-          alt="new-features"
+          src={whats_new || "/placeholder.svg"}
+          alt="ventajas"
           placeholder="blur"
           className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
     </motion.div>
   </section>
-);
+)
 
-export default WhatsNew;
+export default WhatsNew

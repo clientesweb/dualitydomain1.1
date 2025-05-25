d20ba3b5e3b6c564304b03a48127c58a8cdb75aa
clientesweb <alexisifran32@gmail.com>
2025-05-24 21:52:48 -0300

    Actualizar World.js

--- sections/World.jsx
+++ sections/World.jsx
@@ -16,7 +16,7 @@ const World = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title="| People On The World " textStyles="text-center" />
+      <TypingText title="| Es momento para destacar " textStyles="text-center" />
       <TitleText
         title={
           <>
